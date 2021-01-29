d72792783189e3b81d980f83767d52dc848f6933
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-01-29 10:28:47 +0200

    Updating exercise 5 to use String matrix

--- Java Advanced/Lecture 04 - Matrices Exercises/src/Lab05MatrixShuffling.java
+++ Java Advanced/Lecture 04 - Matrices Exercises/src/Lab05MatrixShuffling.java
@@ -1,2 +1,95 @@
-package PACKAGE_NAME;public class Lab05MatrixShuffling {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Lab05MatrixShuffling {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String sizeAsString = scanner.nextLine();
+
+        while (invalidInput(sizeAsString)) {
+            sizeAsString = scanner.nextLine();
+        }
+        int[] size = readArray(sizeAsString, "\\s+");
+        String[][] matrix = readMatrix(size[0], size[1], scanner);
+        String input = scanner.nextLine();
+        while (!input.equals("END")) {
+            String command = input.substring(0, 4);
+            if (!command.equals("swap")) {
+                System.out.println("Invalid input!");
+                input = scanner.nextLine();
+                continue;
+            }
+            input = input.replace("swap", "").trim();
+            if (invalidInput(input)) {
+                System.out.println("Invalid input!");
+                input = scanner.nextLine();
+                continue;
+            }
+
+            boolean isValid = true;
+            int[] coordinates = readArray(input, "\\s+");
+            for (int number = 0; number < coordinates.length; number++) {
+                if (number % 2 == 0) {
+                    if (coordinates[number] >= matrix.length || coordinates[number] < 0) {
+                        isValid = false;
+                        break;
+                    }
+                } else {
+                    if (coordinates[number] >= matrix[0].length || coordinates[number] < 0) {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!isValid) {
+                System.out.println("Invalid input!");
+                input = scanner.nextLine();
+                continue;
+            }
+            String oldString = matrix[coordinates[0]][coordinates[1]];
+            matrix[coordinates[0]][coordinates[1]] = matrix[coordinates[2]][coordinates[3]];
+            matrix[coordinates[2]][coordinates[3]] = oldString;
+            printMatrix(matrix);
+
+            input = scanner.nextLine();
+        }
+    }
+
+    private static boolean invalidInput(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if (!Character.isDigit(currentChar) && currentChar != ' ') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int[] readArray(Scanner scanner, String pattern) {
+        return Arrays.stream(scanner.nextLine().split(pattern))
+                .mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static int[] readArray(String string, String pattern) {
+        return Arrays.stream(string.split(pattern))
+                .mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static String[][] readMatrix(int rows, int cols, Scanner scanner) {
+        String[][] matrix = new String[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            matrix[row] = scanner.nextLine().split("\\s+");
+        }
+        return matrix;
+    }
+
+    private static void printMatrix(String[][] matrix) {
+        for (String[] arr : matrix) {
+            for (String s : arr) {
+                System.out.print(s + " ");
+            }
+            System.out.println();
+        }
+    }
 }
