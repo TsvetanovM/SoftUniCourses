4351c89ae14b7888b805df74407be1f160fde0ab
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-01-30 22:59:37 +0200

    Adding exercise 3 - Voina

--- Java Advanced/LEcture 07 - Sets and Maps Advanced/src/Lab02SoftUniParty.java
+++ Java Advanced/LEcture 07 - Sets and Maps Advanced/src/Lab02SoftUniParty.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Lab02SoftUniParty {
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Lab02SoftUniParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<String> partyList = new TreeSet<>();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("PARTY")) {
+            partyList.add(input);
+            input = scanner.nextLine();
+        }
+
+        input = scanner.nextLine();
+
+        while (!input.equals("END")) {
+            partyList.remove(input);
+            input = scanner.nextLine();
+        }
+
+        System.out.println(partyList.size());
+        for (String guest : partyList) {
+            System.out.println(guest);
+        }
+    }
 }
