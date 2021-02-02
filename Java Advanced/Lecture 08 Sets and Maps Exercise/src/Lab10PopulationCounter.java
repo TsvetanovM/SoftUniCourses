c606b6684db583ced7cb973a9198fb60d7cc986c
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-02-02 12:21:42 +0200

    Updated exercise 10 to not exceed max Integer value

--- Java Advanced/Lecture 08 Sets and Maps Exercise/src/Lab10PopulationCounter.java
+++ Java Advanced/Lecture 08 Sets and Maps Exercise/src/Lab10PopulationCounter.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class Lab10PopulationCounter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<String , LinkedHashMap<String, Integer>> allCountries = new LinkedHashMap<>();
+        Map<String , LinkedHashMap<String, Long>> allCountries = new LinkedHashMap<>();
         String input = scanner.nextLine();
 
         while (!input.equals("report")) {
             String[] info = input.split("\\|");
             allCountries.putIfAbsent(info[1], new LinkedHashMap<>());
-            allCountries.get(info[1]).put(info[0], Integer.parseInt(info[2]));
+            allCountries.get(info[1]).put(info[0], Long.parseLong(info[2]));
             input = scanner.nextLine();
         }
 
         allCountries.entrySet().stream().
-                sorted((e1, e2) -> Integer.compare(e2.getValue().values().stream().mapToInt(Integer::intValue).sum(),
-                        e1.getValue().values().stream().mapToInt(Integer::intValue).sum()))
+                sorted((e1, e2) -> Long.compare(e2.getValue().values().stream().mapToLong(Long::longValue).sum(),
+                        e1.getValue().values().stream().mapToLong(Long::longValue).sum()))
                 .forEach(entry -> {
                     System.out.printf("%s (total population: %d)%n", entry.getKey(),
-                            entry.getValue().values().stream().mapToInt(Integer::intValue).sum());
+                            entry.getValue().values().stream().mapToLong(Long::longValue).sum());
                     entry.getValue().entrySet().stream().sorted((i1, i2) -> i2.getValue().compareTo(i1.getValue()))
                             .forEach(e -> System.out.printf("=>%s: %d%n", e.getKey(), e.getValue()));
                 });
