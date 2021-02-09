8f28f2c5d9667a9da3662be8af6827b2ce233682
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-02-09 15:19:21 +0200

    Updating Exercise 04  - Add VAT.

--- Java Advanced/Lecutre11 - Functional Programming/src/Lab04_AddVAT.java
+++ Java Advanced/Lecutre11 - Functional Programming/src/Lab04_AddVAT.java
@@ -1,18 +1,15 @@
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.function.DoubleUnaryOperator;
-import java.util.function.UnaryOperator;
 
 public class Lab04_AddVAT {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        UnaryOperator<Double> addVAT = e -> e * 1.20;
         System.out.println("Prices with VAT:");
         Arrays.stream(input
                 .split(",\\s+"))
                 .mapToDouble(Double::parseDouble)
-                .map((DoubleUnaryOperator) addVAT)
+                .map(n -> n = n * 1.2)
                 .forEach(e -> System.out.printf("%.2f%n", e));
     }
 }
