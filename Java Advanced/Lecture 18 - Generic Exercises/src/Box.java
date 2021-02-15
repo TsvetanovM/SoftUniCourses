6e5537e5b93f9620ad443c7163f6c684b08bcc8b
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-02-15 11:15:33 +0200

    Updated Class Box for exercise 03 - Generic Swap Method String

--- Java Advanced/Lecture 18 - Generic Exercises/src/Box.java
+++ Java Advanced/Lecture 18 - Generic Exercises/src/Box.java
@@ -1,8 +1,24 @@
+import java.util.List;
+
 public class Box<T> {
     private T t;
 
     public Box(T t) {
         this.t = t;
+    }
+
+    public static <T> void printElements(List<T> list, int firstIndex, int secondIndex) {
+        swapIndexes(list, firstIndex, secondIndex);
+        list.forEach(System.out::println);
+    }
+
+    private static <T> void swapIndexes(List<T> list, int firstIndex, int secondIndex) {
+        T t1 = list.get(firstIndex);
+        T t2 = list.get(secondIndex);
+        list.remove(firstIndex);
+        list.add(firstIndex, t2);
+        list.remove(secondIndex);
+        list.add(secondIndex, t1);
     }
 
     @Override
--- Java Advanced/Lecture 18 - Generic Exercises/src/Main.java
+++ Java Advanced/Lecture 18 - Generic Exercises/src/Main.java
@@ -1,14 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
+        List<Box> boxes = new ArrayList<>();
 
         while (n-- > 0) {
-            int input = Integer.parseInt(scanner.nextLine());
+            String input = scanner.nextLine();
             Box box = new Box(input);
-            System.out.println(box.toString());
+            boxes.add(box);
         }
+
+        int[] indexes = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        Box.printElements(boxes, indexes[0], indexes[1]);
     }
 }
