747f11c6c499767f9738203c3c68a6305b04b400
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-02-16 11:31:17 +0200

    Created class Book for Lab 01 in Iterators and Comparators - Book

--- Java Advanced/Lecture 18 - Generic Exercises/src/Main.java
+++ Java Advanced/Lecture 18 - Generic Exercises/src/Main.java
@@ -7,18 +7,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
-        List<Box> boxes = new ArrayList<>();
+        List<Double> list = new ArrayList<>();
 
         while (n-- > 0) {
-            int input = Integer.parseInt(scanner.nextLine());
-            Box box = new Box(input);
-            boxes.add(box);
+            double input = Double.parseDouble(scanner.nextLine());
+//            Box box = new Box(input);
+            list.add(input);
         }
 
-        int[] indexes = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        double comparator = Double.parseDouble(scanner.nextLine());
+        System.out.println(Box.elementsGreaterThanCount(list, comparator));
 
-        Box.printElements(boxes, indexes[0], indexes[1]);
     }
 }
