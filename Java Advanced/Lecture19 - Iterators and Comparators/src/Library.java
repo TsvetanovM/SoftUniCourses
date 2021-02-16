b62991b1ae60311d7d6a5225e8bce278cf227549
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-02-16 12:23:38 +0200

    Created class Library for Lab 02 in Iterators and Comparators - Library

---
+++ Java Advanced/Lecture19 - Iterators and Comparators/src/Library.java
@@ -0,0 +1,30 @@
+import java.util.Iterator;
+
+public class Library implements Iterable<Book> {
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+
+        return new LibraryIterator();
+    }
+
+    private class LibraryIterator implements Iterator<Book> {
+
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < books.length;
+        }
+
+        @Override
+        public Book next() {
+            return books[index++];
+        }
+    }
+}
--- Java Advanced/Lecture19 - Iterators and Comparators/src/Main.java
+++ Java Advanced/Lecture19 - Iterators and Comparators/src/Main.java
@@ -1,15 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
-        Book bookThree = new Book("The Documents in the Case", 2002);
-        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+        Library library = new Library(new Book("Lord of the Rings", 1938, "Tolkien"),
+                new Book("Way of Kings", 2010, "Brandon Sanderson"),
+                new Book("Words of Radiance", 2013));
 
-        List<Book> books = new ArrayList<>();
-        books.add(bookOne);
-        books.add(bookTwo);
-        books.add(bookThree);
+        for (Book book : library) {
+            System.out.println(book);
+        }
     }
 }
