b7b1febcf10ca0e1b3abb72db025e7e745752728
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-02-18 14:07:05 +0200

    Created Package bakery and classes Bakery, Employee and Main to solve problem 03 - Opening.

---
+++ Java Advanced/Java Exam Practice - old retake exam 16.12.2020/src/bakery/Bakery.java
@@ -0,0 +1,55 @@
+package bakery;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Bakery {
+    private List<Employee> employees;
+    private String name;
+    private int capacity;
+
+    public Bakery(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        this.employees = new ArrayList<>();
+    }
+
+    public void add(Employee employee) {
+        if (this.employees.size() < capacity){
+            this.employees.add(employee);
+        }
+    }
+
+    public boolean remove(String name) {
+        return employees.remove(name);
+    }
+
+    public Employee getOldestEmployee() {
+        List<Employee> list = employees.stream()
+                .sorted((e1, e2) -> Integer.compare(e2.getAge(), e1.getAge()))
+                .collect(Collectors.toList());
+        return list.get(0);
+    }
+
+    public Employee getEmployee(String name) {
+        for (Employee employee : employees) {
+            if (employee.getName().equals(name)){
+                return employee;
+            }
+        };
+        return null;
+    }
+
+    public int getCount() {
+        return employees.size();
+    }
+
+    public String report() {
+       String output = String.format("Employees working at Bakery %s:%n", this.name);
+        for (Employee employee : employees) {
+            output += employee.toString();
+        }
+        return output;
+    }
+}
