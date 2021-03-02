d96a082bd6a170b31fb2a8e119f1b9e9176eacd6
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-03-02 10:09:50 +0200

    Created enum CardRanks for problem 02 - Card Ranks

---
+++ Java OOP/Lecture 01 - Working with Abstraction/src/DeckOfCards/CardRanks.java
@@ -0,0 +1,17 @@
+package DeckOfCards;
+
+public enum CardRanks {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
