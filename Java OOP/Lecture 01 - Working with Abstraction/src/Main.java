e7e577c158a7bc7872ff0fa1730d4591e18db098
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-03-02 11:59:20 +0200

    Refactored class Main for problem 05 - Jedi Academy

--- Java OOP/Lecture 01 - Working with Abstraction/src/Main.java
+++ Java OOP/Lecture 01 - Working with Abstraction/src/Main.java
@@ -1,32 +1,60 @@
-import Calendar.WeekDay;
-import Geometry.*;
-import IOUtils.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    private static int[][] matrix;
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        WeekDay monday = WeekDay.MONDAY;
+        int[] dimensions = parseIntArray(scanner.nextLine());
+        int x = dimensions[0];
+        int y = dimensions[1];
 
-        System.out.println(monday.getNumericRepresentation());
-        System.out.println(monday.ordinal());
+        matrix = new int[x][y];
 
-        WeekDay[] values = WeekDay.values();
-
-        for (WeekDay value : values) {
-            System.out.println(value.getName());
+        int value = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = value++;
+            }
         }
-//        ConsoleReader reader = new ConsoleReader();
-//
-//        int[] coordinates = InputParser.parseArray(reader.readLine(), "\\s+");
-//
-//        Rectangle rectangle = GeometryFactory.createRectangle(coordinates);
-//
-//        int N = Integer.parseInt(reader.readLine());
-//
-//        while (N-- > 0) {
-//            int[] currentCoordinates = InputParser.parseArray(reader.readLine(), "\\s+");
-//            Point point = GeometryFactory.createPoint(currentCoordinates);
-//            System.out.println(rectangle.contains(point));
-//        }
+
+        String command = scanner.nextLine();
+        long sum = 0;
+        while (!command.equals("Let the Force be with you")) {
+            int[] ivoStartingPosition = parseIntArray(command);
+            int[] evilStartingPosition = parseIntArray(scanner.nextLine());
+            int evilRow = evilStartingPosition[0] - 1;
+            int evilCol = evilStartingPosition[1] - 1;
+
+            while (isWithinMatrix(evilRow, evilCol)) {
+                matrix[evilRow][evilCol] = 0;
+                evilRow--;
+                evilCol--;
+            }
+
+            int ivoRow = ivoStartingPosition[0] - 1;
+            int ivoCol = ivoStartingPosition[1] + 1;
+
+            while (isWithinMatrix(ivoRow, ivoCol)) {
+                sum += matrix[ivoRow][ivoCol];
+                ivoCol++;
+                ivoRow--;
+            }
+
+            command = scanner.nextLine();
+        }
+
+        System.out.println(sum);
+
+    }
+
+    private static boolean isWithinMatrix(int row, int col) {
+        return row >= 0 && row < matrix.length && col >= 0 && col < matrix[0].length;
+    }
+
+    private static int[] parseIntArray(String s) {
+        return Arrays.stream(s.split("\\s+")).mapToInt(Integer::parseInt).toArray();
     }
 }
