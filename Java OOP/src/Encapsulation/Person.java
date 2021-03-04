369b16261bf6d02408bd3bfa862c8242687fea58
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-03-04 14:39:03 +0200

    Refactored and expanded Class Person for Exercise 02 - Salary Increase

--- Java OOP/src/Encapsulation/Person.java
+++ Java OOP/src/Encapsulation/Person.java
@@ -4,11 +4,13 @@ public class Person {
     private String firstName;
     private String lastName;
     private int age;
+    private double salary;
 
-    public Person(String firstName, String lastName, int age) {
+    public Person(String firstName, String lastName, int age, double salary) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.salary = salary;
     }
 
     public String getFirstName() {
@@ -23,9 +25,25 @@ public class Person {
         return age;
     }
 
+    public double getSalary() {
+        return salary;
+    }
+
+    private void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     @Override
     public String toString() {
         return  firstName + " " + lastName +
-                " is " + age + " years old.";
+                " gets " + this.salary + " leva";
+    }
+
+    public void increaseSalary(double bonus) {
+        if (this.getAge() >= 30) {
+            this.setSalary(this.getSalary() + (this.getSalary() * bonus / 100));
+        } else {
+            this.setSalary(this.getSalary() + (this.getSalary() * bonus / 200));
+        }
     }
 }
