8033d64e66cdeebbe2294af68055c3ab04377dcb
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-03-12 12:24:45 +0200

    Created package BorderControl for Exercise 05 - Border Control

--- Java OOP/src/InterfacesAndAbstraction/Main.java
+++ Java OOP/src/InterfacesAndAbstraction/Main.java
@@ -1,18 +1,31 @@
 package InterfacesAndAbstraction;
 
-import InterfacesAndAbstraction.ferrari.Car;
-import InterfacesAndAbstraction.ferrari.Ferrari;
+import InterfacesAndAbstraction.BorderControl.Citizen;
+import InterfacesAndAbstraction.BorderControl.Identifiable;
+import InterfacesAndAbstraction.BorderControl.Robot;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        List<Identifiable> entrants = new ArrayList<>();
 
-        String name = scanner.nextLine();
+        while (!input.equals("End")) {
+            String[] tokens = input.split("\\s+");
+            Identifiable entrant = (tokens.length == 3) ?
+                    new Citizen(tokens[0], Integer.parseInt(tokens[1]), tokens[2]) :
+                    new Robot(tokens[0], tokens[1]);
+            entrants.add(entrant);
+            input = scanner.nextLine();
+        }
 
-        Car ferrari = new Ferrari(name);
+        String forDetention = scanner.nextLine();
 
-        System.out.println(ferrari.toString());
+        entrants.stream().filter(e -> e.getId().endsWith(forDetention))
+                .forEach(e -> System.out.println(e.getId()));
     }
 }
