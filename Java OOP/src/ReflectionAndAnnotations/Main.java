2b11c3642f4d81b2cc29672e9b80008a66e7dab8
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-03-21 18:01:54 +0200

    Solved Exercise 01 - Reflection

--- Java OOP/src/ReflectionAndAnnotations/Main.java
+++ Java OOP/src/ReflectionAndAnnotations/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static void main(String[] args) {
         Class reflection = Reflection.class;
 
-        System.out.println(reflection.getSimpleName());
+        System.out.println(reflection);
 
         Class superClass = reflection.getSuperclass();
-        System.out.println(superClass.getSimpleName());
+        System.out.println(superClass);
 
         Class[] interfaces = reflection.getInterfaces();
         for (Class anInterface : interfaces) {
-            System.out.println(anInterface.getSimpleName());
+            System.out.println(anInterface);
         }
 
         try {
