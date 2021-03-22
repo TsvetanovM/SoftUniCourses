2f2cd71f1032da3728ec808a1bd77f94baa6b5ec
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-03-22 11:14:35 +0200

    Solved Exercise 02 - Getters and Setters

--- Java OOP/src/ReflectionAndAnnotations/Main.java
+++ Java OOP/src/ReflectionAndAnnotations/Main.java
@@ -1,43 +1,71 @@
 package ReflectionAndAnnotations;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class Main {
+    public static class MethodComparator implements Comparator<Method> {
+
+        @Override
+        public int compare(Method f, Method s) {
+            return f.getName().compareTo(s.getName());
+        }
+    }
+
     public static void main(String[] args) {
-        Class reflection = Reflection.class;
+        Class clazz = Reflection.class;
 
-        System.out.println(reflection);
+//        Constructor<Reflection> ctor = clazz.getDeclaredConstructor();
+//        Reflection reflection = ctor.newInstance();
 
-        Class superClass = reflection.getSuperclass();
-        System.out.println(superClass);
+        Method[] methods = clazz.getDeclaredMethods();
+        Set<Method> setters = new TreeSet<>(new MethodComparator());
+        Set<Method> getters = new TreeSet<>(new MethodComparator());
 
-        Class[] interfaces = reflection.getInterfaces();
-        for (Class anInterface : interfaces) {
-            System.out.println(anInterface);
+        for (Method method : methods) {
+            if (method.getName().startsWith("get")) {
+                getters.add(method);
+            } else if (method.getName().startsWith("set")) {
+                setters.add(method);
+            }
         }
 
-        try {
-            Constructor<Reflection> declaredConstructor = reflection.getDeclaredConstructor();
-            Reflection reflectionObject = declaredConstructor.newInstance();
-            System.out.println(reflectionObject.toString());
-        } catch (NoSuchMethodException
-                | IllegalAccessException
-                | InstantiationException
-                | InvocationTargetException e) {
-            e.printStackTrace();
-        }
+        getters.forEach(m -> System.out.println( m.getName() + " will return class "
+                + m.getReturnType().getName()));
 
-//        try {
-//            Class clazz = Class.forName("ReflectionAndAnnotations.Reflection");
-//            System.out.println(clazz.getName());
-//            System.out.println(clazz.getSuperclass());
-//            Class[] interfaces = clazz.getInterfaces();
-//            for (Class anInterface : interfaces) {
-//                System.out.println(anInterface.getSimpleName());
-//            }
+        setters.forEach(m -> System.out.println( m.getName() + " and will set field of class "
+                + m.getParameterTypes()[0]));
+//        Class reflection = Reflection.class;
 //
-//        } catch (ClassNotFoundException e) {
+//        System.out.println("class " + reflection.getSimpleName());
+//
+//        Class superClass = reflection.getSuperclass();
+//        System.out.println(superClass);
+//
+//        Class[] interfaces = reflection.getInterfaces();
+//        for (Class anInterface : interfaces) {
+//            System.out.println(anInterface);
+//        }
+//
+//        try {
+//            Constructor<Reflection> declaredConstructor = reflection.getDeclaredConstructor(
+//                    String.class, String.class, String.class
+//            );
+//            declaredConstructor.setAccessible(true);
+//            Reflection reflectionObject = declaredConstructor.newInstance("1", "6", "9");
+//
+//            Field zip = reflection.getDeclaredField("zip");
+//            zip.setAccessible(true);
+//            int oldValue =(int) zip.get(reflectionObject);
+//            System.out.println("Old value: " + oldValue);
+//
+//            zip.set(reflectionObject, 1612);
+//            System.out.println("New value: " + zip.get(reflectionObject));
+//            System.out.println(reflectionObject.toString());
+//        } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException | NoSuchFieldException e) {
 //            e.printStackTrace();
 //        }
     }
