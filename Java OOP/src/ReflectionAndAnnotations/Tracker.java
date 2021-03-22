0206c85e70ac6275d29186603f3db1fcbcec0161
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-03-22 12:30:17 +0200

    Solved Exercise 05 - Coding Tracker

---
+++ Java OOP/src/ReflectionAndAnnotations/Tracker.java
@@ -0,0 +1,32 @@
+package ReflectionAndAnnotations;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Tracker {
+
+    @Author(name = "George")
+    public static void main(String[] args) {
+        Tracker.printMethodsByAuthor(Tracker.class);
+    }
+
+    @Author(name = "Peter")
+    public static void printMethodsByAuthor(Class<?> cl) {
+        Method[] methods = cl.getDeclaredMethods();
+
+        Map<String, Method> methodsByAuthor = new TreeMap<>();
+
+        for (Method method : methods) {
+            Author author = method.getAnnotation(Author.class);
+
+            if (author != null) {
+                methodsByAuthor.put(author.name(), method);
+            }
+        }
+
+        for (Map.Entry<String, Method> entry : methodsByAuthor.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue().getName() + "()");
+        }
+    }
+}
