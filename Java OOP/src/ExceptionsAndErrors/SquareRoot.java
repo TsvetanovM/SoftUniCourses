7d584f4e8f481f9d12a655e9e01e4a00b7e8e576
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-03-25 12:00:29 +0200

    Created class SquareRoot for Exercise 01 - Square Root

---
+++ Java OOP/src/ExceptionsAndErrors/SquareRoot.java
@@ -0,0 +1,20 @@
+package ExceptionsAndErrors;
+
+import java.util.IllegalFormatException;
+import java.util.Scanner;
+
+public class SquareRoot {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            int num = scanner.nextInt();
+            System.out.println(Math.sqrt(num));
+        } catch (Exception e) {
+            System.out.println("Invalid number");
+        } finally {
+            System.out.println("Good bye");
+        }
+    }
+}
