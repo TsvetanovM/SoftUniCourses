3efcd93dfd3f3ba6203f412f8bc599c6680e04b5
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-03-28 00:03:33 +0200

    Created class DummyTest for exercise 03 - Test Dummy Tested Dummy loses health if attacked, Dead Dummy throws exception when attacked, Dead Dummy gives XP, Alive Dummy can't give XP

--- Java OOP/UnitTestingMaven/src/test/java/DummyTest.java
+++ Java OOP/UnitTestingMaven/src/test/java/DummyTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import rpg_lab.Dummy;
 
@@ -6,41 +7,37 @@ import static org.junit.Assert.assertEquals;
 public class DummyTest {
     private static final int EXPERIENCE = 100;
     private static final int ATTACK_POINTS = 10;
-    private static final int STARTING_HEALTH = 100;
+    private static final int STARTING_HEALTH_ALIVE = 100;
+    private static final int STARTING_HEALTH_DEAD = 0;
 
-    private Dummy dummy;
+    private Dummy aliveDummy;
+    private Dummy deadDummy;
+
+    @Before
+    public void setUp() {
+        this.aliveDummy = new Dummy(STARTING_HEALTH_ALIVE, EXPERIENCE);
+        this.deadDummy = new Dummy(STARTING_HEALTH_DEAD, EXPERIENCE);
+    }
 
     @Test
-    public void testDummyLosesHealthIfAttackedAndIsNotAlreadyDead() {
-        dummy = createDummy();
-        dummy.takeAttack(ATTACK_POINTS);
-        assertEquals(STARTING_HEALTH - ATTACK_POINTS, dummy.getHealth());
+    public void testDummyLosesHealthWhenAttackedAndIsNotAlreadyDead() {
+        aliveDummy.takeAttack(ATTACK_POINTS);
+        assertEquals(STARTING_HEALTH_ALIVE - ATTACK_POINTS, aliveDummy.getHealth());
     }
 
     @Test(expected = IllegalStateException.class)
-    public void testDeadDummyThrowsExceptionWhenAttacked() {
-        dummy = createDeadDummy();
-        dummy.takeAttack(ATTACK_POINTS);
+    public void testDeadDummyFailsWhenAttacked() {
+        deadDummy.takeAttack(ATTACK_POINTS);
     }
 
     @Test
-    public void testDeadDummyGivesXP() {
-        dummy = createDeadDummy();
-        int returnedExperience = dummy.giveExperience();
+    public void testDeadDummyGivesExperience() {
+        int returnedExperience = deadDummy.giveExperience();
         assertEquals(EXPERIENCE, returnedExperience);
     }
 
     @Test(expected = IllegalStateException.class)
-    public void testAliveDummyCannotGiveXP() {
-        dummy = createDummy();
-        dummy.giveExperience();
-    }
-
-    private Dummy createDummy() {
-        return new Dummy(STARTING_HEALTH, EXPERIENCE);
-    }
-
-    private Dummy createDeadDummy() {
-        return new Dummy(0, EXPERIENCE);
+    public void testAliveDummyDoesNotGiveExperience() {
+        aliveDummy.giveExperience();
     }
 }
