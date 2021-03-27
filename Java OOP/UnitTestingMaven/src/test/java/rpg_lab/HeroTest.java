42945efcd50f6c46c40178dc7093d30d9c5fe700
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-03-28 00:55:00 +0200

    Refactored HeroTest to use mocking instead of anonymous fake objects.

--- Java OOP/UnitTestingMaven/src/test/java/rpg_lab/HeroTest.java
+++ Java OOP/UnitTestingMaven/src/test/java/rpg_lab/HeroTest.java
@@ -3,6 +3,7 @@ package rpg_lab;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class HeroTest {
     private static final int EXPECTED_EXPERIENCE = 420;
@@ -10,46 +11,12 @@ public class HeroTest {
     @Test
     public void testHeroGainsExperienceWhenTargetDies() {
 
-        Weapon weapon = new Weapon() {
-            @Override
-            public void attack(Target target) {
-
-            }
-
-            @Override
-            public int getAttackPoints() {
-                return 0;
-            }
-
-            @Override
-            public int getDurabilityPoints() {
-                return 0;
-            }
-        };
-
-        Target target = new Target() {
-            @Override
-            public int getHealth() {
-                return 0;
-            }
-
-            @Override
-            public void takeAttack(int attackPoints) {
-
-            }
-
-            @Override
-            public int giveExperience() {
-                return EXPECTED_EXPERIENCE;
-            }
-
-            @Override
-            public boolean isDead() {
-                return true;
-            }
-        };
-
+        Weapon weapon = mock(Weapon.class);
         Hero hero = new Hero("Batko", weapon);
+
+        Target target = mock(Target.class);
+        when(target.isDead()).thenReturn(true);
+        when(target.giveExperience()).thenReturn(EXPECTED_EXPERIENCE);
         hero.attack(target);
         assertEquals(EXPECTED_EXPERIENCE, hero.getExperience());
     }
