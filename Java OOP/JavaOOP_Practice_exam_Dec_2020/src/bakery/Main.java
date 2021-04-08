71ceb4f3633fbf5847e1f6abd6bdf112883f9efa
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-04-08 11:43:28 +0300

    Solved Exercise 2 for Exam December 2020

--- Java OOP/JavaOOP_Practice_exam_Dec_2020/src/bakery/Main.java
+++ Java OOP/JavaOOP_Practice_exam_Dec_2020/src/bakery/Main.java
@@ -25,7 +25,6 @@ public class Main {
 
         Controller controller = new ControllerImpl(foodRepository, drinkRepository, tableRepository);
 
-        // TODO:OPTIONAL
         ConsoleReader reader = new ConsoleReader();
         ConsoleWriter writer = new ConsoleWriter();
         EngineImpl engine = new EngineImpl(reader, writer, controller);
--- Java OOP/JavaOOP_Practice_exam_Dec_2020/src/bakery/core/ControllerImpl.java
+++ Java OOP/JavaOOP_Practice_exam_Dec_2020/src/bakery/core/ControllerImpl.java
@@ -3,17 +3,19 @@ package bakery.core;
 import bakery.common.ExceptionMessages;
 import bakery.common.OutputMessages;
 import bakery.core.interfaces.Controller;
-import bakery.entities.bakedFoods.Bread;
-import bakery.entities.bakedFoods.Cake;
+import bakery.entities.bakedFoods.*;
 import bakery.entities.bakedFoods.interfaces.BakedFood;
+import bakery.entities.drinks.*;
 import bakery.entities.drinks.interfaces.Drink;
+import bakery.entities.tables.InsideTable;
+import bakery.entities.tables.OutsideTable;
 import bakery.entities.tables.interfaces.Table;
 import bakery.repositories.DrinkRepositoryImpl;
 import bakery.repositories.FoodRepositoryImpl;
 import bakery.repositories.TableRepositoryImpl;
 import bakery.repositories.interfaces.*;
 
-import static bakery.common.ExceptionMessages.FOOD_OR_DRINK_EXIST;
+import static bakery.common.ExceptionMessages.*;
 import static bakery.common.OutputMessages.*;
 
 public class ControllerImpl implements Controller {
@@ -46,20 +48,43 @@ public class ControllerImpl implements Controller {
 
     @Override
     public String addDrink(String type, String name, int portion, String brand) {
-        //TODO:
-        return null;
+        if (drinkRepository.getAll().stream()
+                .anyMatch(f -> f.getName().equals(name))) {
+            throw new IllegalArgumentException(String.format(FOOD_OR_DRINK_EXIST, type, name));
+        }
+
+        if (type.equals("Tea")) {
+            drinkRepository.add(new Tea(name, portion, brand));
+        } else {
+            drinkRepository.add(new Water(name, portion, brand));
+        }
+
+        return String.format(DRINK_ADDED, name, brand);
     }
 
     @Override
     public String addTable(String type, int tableNumber, int capacity) {
-        //TODO:
-        return null;
+       if (tableRepository.getAll().stream()
+       .anyMatch(t -> t.getTableNumber() == tableNumber)) {
+           throw new IllegalArgumentException(String.format(TABLE_EXIST, tableNumber));
+       }
+       if (type.equals("InsideTable")) {
+           tableRepository.add(new InsideTable(tableNumber, capacity));
+       } else {
+           tableRepository.add(new OutsideTable(tableNumber, capacity));
+       }
+       return String.format(TABLE_ADDED, tableNumber);
     }
 
     @Override
     public String reserveTable(int numberOfPeople) {
-        //TODO:
-        return null;
+        for (Table table : tableRepository.getAll()) {
+            if (table.getCapacity() >= numberOfPeople) {
+                table.reserve(numberOfPeople);
+                return String.format(TABLE_RESERVED, table.getTableNumber(), numberOfPeople);
+            }
+        }
+        return String.format(RESERVATION_NOT_POSSIBLE, numberOfPeople);
     }
 
     @Override
