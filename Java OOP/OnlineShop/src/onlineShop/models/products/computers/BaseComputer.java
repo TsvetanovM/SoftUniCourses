2a3f1c7669c49ad5b77179c2f92bac3d527c783c
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-05-19 11:15:40 +0300

    Added structure to the project (Exercise 1)

---
+++ Java OOP/OnlineShop/src/onlineShop/models/products/computers/BaseComputer.java
@@ -0,0 +1,56 @@
+package onlineShop.models.products.computers;
+
+
+import onlineShop.models.products.BaseProduct;
+import onlineShop.models.products.components.Component;
+import onlineShop.models.products.peripherals.Peripheral;
+
+import java.util.List;
+
+public abstract class BaseComputer extends BaseProduct implements Computer {
+
+    private List<Component> components;
+    private List<Peripheral> peripherals;
+
+    public BaseComputer(int id, String manufacturer, String model, double price, double overallPerformance) {
+        super(id, manufacturer, model, price, overallPerformance);
+    }
+
+    @Override
+    public List<Component> getComponents() {
+        return null;
+    }
+
+    @Override
+    public List<Peripheral> getPeripherals() {
+        return null;
+    }
+
+    @Override
+    public void addComponent(Component component) {
+
+    }
+
+    @Override
+    public Component removeComponent(String componentType) {
+        return null;
+    }
+
+    @Override
+    public void addPeripheral(Peripheral peripheral) {
+
+    }
+
+    @Override
+    public Peripheral removePeripheral(String peripheralType) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseComputer{" +
+                "components=" + components +
+                ", peripherals=" + peripherals +
+                '}';
+    }
+}
