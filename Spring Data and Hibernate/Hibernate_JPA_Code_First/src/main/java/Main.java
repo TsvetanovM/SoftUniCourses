d4679ae1ba73aa96930ace49f2012ca24f78ef4d
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-07-09 12:58:05 +0300

    Building different simple databases with various relations and inheritance style based on the exercises in Lecture 09 - Code First Hibernate

--- Spring Data and Hibernate/Hibernate_JPA_Code_First/src/main/java/Main.java
+++ Spring Data and Hibernate/Hibernate_JPA_Code_First/src/main/java/Main.java
@@ -7,28 +7,7 @@ public class Main {
         System.out.println("Please enter the exercise number:");
         try {
             int exNumber = Integer.parseInt(new Scanner(System.in).nextLine());
-            switch (exNumber) {
-                case 1:
-                    createDatabase("Gringotts - Exercise 01");
-                    break;
-                case 2:
-                    createDatabase("Sales - Exercise 02");
-                    break;
-                case 3:
-                    createDatabase("University - Exercise 03");
-                    break;
-                case 4:
-                    createDatabase("Hospital - Exercise 04");
-                    break;
-                case 5:
-                    createDatabase("Bills Payment System - Exercise 05");
-                    break;
-                case 6:
-                    createDatabase("Football Betting - Exercise 06");
-                    break;
-                default:
-                    System.out.println("No such exercise exists!");
-            }
+            executeExercise(exNumber);
         } catch (NumberFormatException e) {
             System.out.println("Please enter a valid exercise number!");
         }
@@ -38,5 +17,30 @@ public class Main {
         EntityManager em = Persistence
                 .createEntityManagerFactory(persistenceUnitName)
                 .createEntityManager();
+    }
+
+    private static void executeExercise(int exerciseNumber) {
+        switch (exerciseNumber) {
+            case 1:
+                createDatabase("Gringotts - Exercise 01");
+                break;
+            case 2:
+                createDatabase("Sales - Exercise 02");
+                break;
+            case 3:
+                createDatabase("University - Exercise 03");
+                break;
+            case 4:
+                createDatabase("Hospital - Exercise 04");
+                break;
+            case 5:
+                createDatabase("Bills Payment System - Exercise 05");
+                break;
+            case 6:
+                createDatabase("Football Betting - Exercise 06");
+                break;
+            default:
+                System.out.println("No such exercise exists!");
+        }
     }
 }
